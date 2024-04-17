06188fd0728f2977a67acb32706b7dd0abb9edcc
b4baxxx <zidanyyu@gmail.com>
2024-04-17 22:39:13 +0700

    neww

--- Latihan 3/script.js
+++ Latihan 3/script.js
@@ -1,44 +1,42 @@
 let inputnama = document.getElementById("nama");
-let inputtestimoni = document.getElementById("Testimoni");
+let inputtestimoni = document.getElementById("testimoni");
 let btnSimpan = document.getElementById("btnSimpan");
 let list = document.getElementById("list");
 
 let data = [];
 
-btnSimpan.addEventListener("click", function() {
-    if (localStorage.getItem("testimoni")) {
-        // ada localStorange dengan key testimoni
-        data = JSON.parse(localStorage.getItem("testimoni"));
-        data.push({
-            nama: inputnama.value,
-            testimoni: inputtestimoni.value,
-        });
-        console.log(data);
-        
-    } else {
-        // belum ada localStorage dengan key testimoni
-        data.push({
-            nama: inputnama.value,
-            testimoni: inputtestimoni.value,
-
-        });
-        console.log(data);
-    }
-    // simpan ke dalam localStorage
-    localStorage.setItem("testimoni", JSON.stringify(data));
-    getData();
-
+btnSimpan.addEventListener("click", function () {
+  if (localStorage.getItem("testimoni")) {
+    // ada localstorage dengan key testimoni
+    data = JSON.parse(localStorage.getItem("testimoni"));
+    data.push({
+      nama: inputnama.value,
+      testimoni: inputtestimoni.value,
+    });
+  } else {
+    // belum ada localStorage dengan key testimoni
+    data.push({
+      nama: inputnama.value,
+      testimoni: inputtestimoni.value,
+    });
+  }
+  //simpan ke dalam localstorage
+  localStorage.setItem("testimoni", JSON.stringify(data));
+  //panggil getdata()
+  getData();
 });
 
-//tampilkan data localStorage
+// tampilkan data localStorage
 function getData() {
-if (localStorage.getItem("testimoni")) {
-    // ada localStorage dengan key testimoni
+    // clear element list
+    list.innerHTML = "";
+  if (localStorage.getItem("testimoni")) {
+    // ada local storage dengan key testimoni
     data = JSON.parse(localStorage.getItem("testimoni"));
-    for (const[index, row] of data.entries()) {
-        list.innerHTML += `<li>${row.nama} ${row.testimoni}</li>`;
-        }
+    for (const [index, row] of data.entries()) {
+      list.innerHTML += <tr><td>${row.nama}</td><td> ${row.testimoni}</td></tr>;
     }
+  }
 }
-
-
+// panggil getdata()
+getData();
